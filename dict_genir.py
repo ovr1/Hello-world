2ec0b7d894415bb768b221627cc3eaaf2b0a0a2f
ovr1 <rybynok@mail.ru>
2019-01-18 12:02:40 +0300

    TestTestTest

--- dict_genir.py
+++ dict_genir.py
@@ -1,3 +1,5 @@
-f_name = 'registration3'
+f_name = 'registration3.txt'
 
-var = {ix: line for ix, line in enumerate(open(f_name))}
+
+L = {i: line[0:] for i, line in enumerate(open(f_name, encoding='UTF-8'))}
+print(L)
